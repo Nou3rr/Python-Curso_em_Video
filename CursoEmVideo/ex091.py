1fa199dab4383dcd977584a0b0f0b63f28cdd076
Nou3rr <kauannouer@gmail.com>
2026-01-18 17:51:38 -0300

    Mais exercícios executados

--- CursoEmVideo/ex091.py
+++ CursoEmVideo/ex091.py
@@ -2,7 +2,7 @@
 # No final, coloque esse dicionário em ordem, sabendo que o vencedor tirou o maior número no dado.
 
 from random import randint
-from operator import itemgetter
+
 
 jogadores = {}
 jogadores['jogador1'] = randint(1,6)
